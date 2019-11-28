a93f4cba5c4216de8cb9fa4ddce85a0f1da37190
serhii1991 <serhiikleimovych>
2019-11-28 22:13:19 +0100

    added features

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	$(".rating").rateYo({
+    rating: 4.5,
+    starWidth: "15px",
+    halfStar: true,
+    readOnly: true
+  });
+
+  $('.featured__inner').slick({
+  	prevArrow: '<button type="button" class="slick-prev"><span class="icon-angle-left"></span></button>',
+  	nextArrow: '<button type="button" class="slick-next"><span class="icon-angle-right"></span></button>'
+  });
+
+});
