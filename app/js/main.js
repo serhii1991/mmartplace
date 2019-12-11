cdb823d876cc6b550de87cbd212c469088f4573f
serhii1991 <serhiikleimovych>
2019-12-11 07:55:53 +0100

    start products page

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,8 @@ $(function() {
     readOnly: true
   });
 
+  $('input, select').styler();
+
   $('.featured__inner').slick({
   	prevArrow: '<button type="button" class="slick-prev"><span class="icon-angle-left"></span></button>',
   	nextArrow: '<button type="button" class="slick-next"><span class="icon-angle-right"></span></button>'
--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,9 @@ gulp.task('style', function() {
 		'node_modules/normalize.css/normalize.css',
 		'node_modules/slick-carousel/slick/slick.css',
 		'node_modules/magnific-popup/dist/magnific-popup.css',
-		'node_modules/rateyo/src/jquery.rateyo.css'
+		'node_modules/rateyo/src/jquery.rateyo.css',
+		'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
+		'node_modules/jquery-form-styler/dist/jquery.formstyler.css'
 	])
 		.pipe(concat('libs.min.css'))
 		.pipe(cssmin())
@@ -36,7 +38,8 @@ gulp.task('script', function() {
 		'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
 		'node_modules/mixitup/dist/mixitup.js',
 		'node_modules/rateyo/src/jquery.rateyo.js',
-		'node_modules/mixitup/dist/mixitup.js'
+		'node_modules/mixitup/dist/mixitup.js',
+		'node_modules/jquery-form-styler/dist/jquery.formstyler.js'
 	])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
